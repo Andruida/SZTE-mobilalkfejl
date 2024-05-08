5f44513cf7838ddf52902d9e2a4333efbc7e9c5f
Andruida <41823729+Andruida@users.noreply.github.com>
2024-05-08 23:08:43 +0200

    Firestore

--- app/src/main/java/hu/andruida/nezzuk/authentication/LoginActivity.java
+++ app/src/main/java/hu/andruida/nezzuk/authentication/LoginActivity.java
@@ -3,6 +3,9 @@ package hu.andruida.nezzuk.authentication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +13,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+
+import hu.andruida.nezzuk.MainActivity;
 import hu.andruida.nezzuk.R;
 
 public class LoginActivity extends AppCompatActivity {
-    public static final String LOG_TAG = "LoginActivity";
+    private static final String LOG_TAG = "LoginActivity";
+    private FirebaseAuth mAuth;
+
+    private EditText emailEditText;
+    private EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,13 +36,65 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        emailEditText = findViewById(R.id.usernameEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+
+        mAuth = FirebaseAuth.getInstance();
+        if (mAuth.getCurrentUser() != null) {
+            Log.e(LOG_TAG, "User is already logged in, redirecting to main page");
+            startMainActivity();
+        }
+
         if (getSupportActionBar() != null) {
             getSupportActionBar().setTitle(R.string.login_page_name);
         }
         Log.i(LOG_TAG, "Action bar:" + getSupportActionBar());
+
+
         findViewById(R.id.registerButton).setOnClickListener(v -> {
             Intent intent = new Intent(this, RegisterActivity.class);
             startActivity(intent);
         });
+
+        findViewById(R.id.loginButton).setOnClickListener(login);
+        findViewById(R.id.anonLoginButton).setOnClickListener(loginAsAnon);
     }
+
+    private void startMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(i);
+        finish();
+    }
+
+    private final View.OnClickListener login = v -> {
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        if (email.isEmpty() || password.isEmpty()) return;
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    startMainActivity();
+                } else {
+                    Log.e(LOG_TAG, "User login failed", task.getException());
+                    Toast.makeText(this, R.string.login_failed, Toast.LENGTH_SHORT).show();
+                }
+            });
+    };
+
+    private final View.OnClickListener loginAsAnon = v -> {
+        mAuth.signInAnonymously()
+            .addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    startMainActivity();
+                } else {
+                    Log.e(LOG_TAG, "Anonymous login failed", task.getException());
+                    Toast.makeText(this, R.string.login_failed, Toast.LENGTH_SHORT).show();
+                }
+            });
+    };
+
+
+
 }
--- app/src/main/java/hu/andruida/nezzuk/authentication/RegisterActivity.java
+++ app/src/main/java/hu/andruida/nezzuk/authentication/RegisterActivity.java
@@ -1,18 +1,32 @@
 package hu.andruida.nezzuk.authentication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import hu.andruida.nezzuk.MainActivity;
 import hu.andruida.nezzuk.R;
 
 public class RegisterActivity extends AppCompatActivity {
     public static final String LOG_TAG = "RegisterActivity";
+    private FirebaseAuth mAuth;
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private EditText passwordAgainEditText;
+    private EditText usernameEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,9 +38,59 @@ public class RegisterActivity extends AppCompatActivity {
             return insets;
         });
 
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        passwordAgainEditText = findViewById(R.id.passwordAgainEditText);
+        usernameEditText = findViewById(R.id.usernameEditText);
+
+        mAuth = FirebaseAuth.getInstance();
+        if (mAuth.getCurrentUser() != null) {
+            Log.e(LOG_TAG, "User is already logged in, redirecting to main page");
+            startMainActivity();
+        }
+
         if (getSupportActionBar() != null) {
             getSupportActionBar().setTitle(R.string.register_page_name);
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+
+        findViewById(R.id.registerButton).setOnClickListener(register);
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                finish();
+            }
+        });
     }
+
+    private final View.OnClickListener register = v -> {
+        findViewById(R.id.registerButton).setEnabled(false);
+        if (!passwordEditText.getText().toString().equals(passwordAgainEditText.getText().toString())) {
+            Log.e(LOG_TAG, "Passwords do not match");
+            Toast.makeText(this, R.string.passwords_do_not_match, Toast.LENGTH_SHORT).show();
+            findViewById(R.id.registerButton).setEnabled(true);
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(emailEditText.getText().toString(), passwordEditText.getText().toString())
+            .addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    Log.i(LOG_TAG, "Registration successful");
+                    Toast.makeText(this, R.string.successful_registration, Toast.LENGTH_SHORT).show();
+                    startMainActivity();
+                } else {
+                    Log.e(LOG_TAG, "Registration failed", task.getException());
+                    findViewById(R.id.registerButton).setEnabled(true);
+                    Toast.makeText(this, R.string.failed_registration, Toast.LENGTH_SHORT).show();
+                }
+            });
+    };
+
+    private void startMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(i);
+        finish();
+    }
+
 }
